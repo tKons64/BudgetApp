bbc6f5db82f125c1033d704985aecd36ef516cb4
TretyakovKonstantin <spb_unex@mail.ru>
2022-12-13 19:58:03 +0300

    Урок 2. Домашние задание

---
+++ src/main/java/me/tretyakovv/budgetapp/controllers/FirstControllers.java
@@ -0,0 +1,23 @@
+package me.tretyakovv.budgetapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstControllers {
+
+    @GetMapping()
+    public String helloWorld() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String page() {
+        return "Третьяков Константин"
+                + ", программа - Бюджет семьи"
+                + ", дата: 09.12.2022"
+                + ", программа для ведения бюджета"
+                ;
+    }
+
+}
